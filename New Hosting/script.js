dc14a845370b62e34c1cd633caac753812a7c51a
flrten <131455762+flrten@users.noreply.github.com>
2023-04-21 14:51:29 +0300

    Add files via upload

---
+++ New Hosting/script.js
@@ -0,0 +1,10 @@
+<script>
+  // получаем кнопку
+  var freeButton = document.getElementById("freeButton");
+
+  // добавляем обработчик события "click" на кнопку
+  freeButton.addEventListener("click", function() {
+    // перенаправляем пользователя на другую страницу
+    window.location.href = "free.html";
+  });
+</script>
